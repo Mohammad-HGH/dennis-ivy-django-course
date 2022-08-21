53816371632674da4e770f4b4a16043cb3bb098a
Mohammad Nazari <mohammadnzrhelia@gmail.com>
2022-08-21 09:58:34 +0430

    add dynamic pages and pass params to them

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,37 @@
+
 from django.shortcuts import render
 
 
 # Create your views here.
 
+projectList = [
+    {
+        "id": "1",
+        "title": "Ecommerce Website",
+        "description": "Fully functional ecommerce website",
+    },
+    {
+        "id": "2",
+        "title": "Portfolio Website",
+        "description": "This is a personal website",
+    },
+    {
+        "id": "3",
+        "title": "Social network",
+        "description": "Awesome open source social media",
+    },
+]
+
 
 def projects(request):
-    return render(request, "projects/projects.html")
+    pages = projectList
+    return render(request, "projects/projects.html", {"pages": pages})
 
 
 def project(request, pk):
-    return render(request, "projects/single-project.html")
+    projectObj = None
+    for i in projectList:
+        if i["id"] == pk:
+            projectObj = i
+
+    return render(request, "projects/single-project.html", {"project": projectObj})
