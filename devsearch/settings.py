529c463a3e1dbfcf46e785fab2c4d04cbcf43e4a
Mohammad Nazari <mohammadnzrhelia@gmail.com>
2022-08-24 13:04:17 +0430

    optimize code

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -124,15 +124,12 @@ USE_TZ = True
 
 MEDIA_URL = '/upload/'
 STATIC_URL = '/static/'
-# /static/images/img1.jpg
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "upload", "static"),
-    # '/var/www/static/',
+    BASE_DIR / "static",
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "devsearch/static", "../static/")
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "devsearch/upload", "../upload/img")
+MEDIA_ROOT = os.path.join(BASE_DIR, "upload", "img")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- projects/models.py
+++ projects/models.py
@@ -34,7 +34,7 @@ class Project(models.Model):
     id = models.UUIDField(
         default=uuid.uuid4, unique=True, primary_key=True, editable=False
     )
-    featured_image = models.ImageField(null=True, blank=True, default="default.jpg")
+    featured_image = models.ImageField(null=True, blank=True, default="default.jpg", upload_to="img")
 
     def __str__(self):
         return self.title
